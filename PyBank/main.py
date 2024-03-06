621692ebc3f95d8e271fa9af32e0b0134dcbee94
Jack Temme <jtemme7@gmail.com>
2024-03-06 12:24:03 -0700

    updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,48 +1,50 @@
 import os
 import csv
 
+# import csv using relative path
 budget_csv = "PyBank/Resources/budget_data.csv"
 
+# set up variables for iterations
 months = []
 profits = []
+profit_change = []
 total_amount = 0
 
-
+# open file to read
 with open(budget_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    next(csvreader, None)
+    header = next(csvreader, None)
 
-
+# loop through csv file and otput the month and profit/losses
     for row in csvreader:
         total_amount = total_amount + int(row[1])
         months.append(row[0])
         profits.append(row[1])
 
-  
-month_count = len(months)
-
-profit_change = []
-
+# from profit list determine profit changes over time
 for x in range(1, len(profits)):
     profit_change.append((int((profits[x])) - (int(profits[x-1]))))
 
-
+# calculate total months and average, max, and min values of profit changes
+month_count = len(months)
 average_change = sum(profit_change) / len(profit_change)
 max_change = max(profit_change)
 min_change = min(profit_change)
 
+# find index for max and min change to reference the date it occured
 date_index = profit_change.index(max_change)
 date_index1 = profit_change.index(min_change) 
 
+# print findings to terminal
 print("Financial Analysis\n-----------------")       
 print(f"Total Months: {month_count}")
 print(f"Total: ${total_amount}")
-print(f"Average: {round(average_change, 2)}")
+print(f"Average: ${round(average_change, 2)}")
 print(f"Greatest Increase in profits: {months[date_index + 1]} (${max_change})")
 print(f"Greatest Decrease in profits: {months[date_index1 + 1]} (${min_change})")
 
+# create a csv file to write results to
 out_file = os.path.join("PyBank/analysis/Profit_Results.csv")
-
 
 with open (out_file,"w", newline="") as file:
     writer = csv.writer(file)
@@ -51,6 +53,6 @@ with open (out_file,"w", newline="") as file:
     writer.writerow(["-------------------"]) 
     writer.writerow([f"Total Months: {month_count}"])
     writer.writerow([f"Total: ${total_amount}"])
-    writer.writerow([f"Average: {round(average_change, 2)}"])
+    writer.writerow([f"Average: ${round(average_change, 2)}"])
     writer.writerow([f"Greatest Increase in profits: {months[date_index + 1]} (${max_change})"])
     writer.writerow([f"Greatest Decrease in profits: {months[date_index1 + 1]} (${min_change})"])
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,42 +1,80 @@
 import os 
 import csv
 
+# import csv using relative path
 election_csv = "PyPoll/Resources/election_data.csv"
 
-charles_votes = 0
-diane_votes = 0
-raymon_votes = 0
-total_votes = 0
 
+# Lists to store candidates who received votes
+candidates = []
+candidates_cleaned = []
+
+# variables for candidates
+# referenced in candidate cleaned list calculated below
+charles_votes = 0
+diana_votes = 0
+raymon_votes = 0
+
+# open file to read
 with open (election_csv, newline="") as file:
     csvreader = csv.reader(file, delimiter = ",")
-    next (csvreader, None)
+    header = next (csvreader, None)
 
-    for row in csvreader:
-        
-        total_votes += 1
+# loop through csv file and add candidates to list
+    for row in csvreader: 
+        candidates.append(row[2])
+
+    # count up votes for each candidate
         if row[2] == "Charles Casper Stockham":
             charles_votes += 1
-        if row[2] == "Diana DeGette":
-            diane_votes += 1
-        if row[2] == "Raymon Anthony Doane":
+        elif row[2] == "Diana DeGette":
+            diana_votes += 1
+        elif row[2] == "Raymon Anthony Doane":
             raymon_votes += 1
 
-charles_percentage = ((charles_votes)/ total_votes) * 100
-diane_percentage = ((diane_votes)/ total_votes) * 100
-raymon_percentage = ((raymon_votes)/ total_votes) * 100
+# "set" function used to determine candidates who received votes
+# reference for candidate variables above          
+candidates_cleaned = list(set(candidates))
 
-if charles_percentage > diane_percentage and raymon_percentage:
+# calculate percentages and total votes
+total_votes = len(candidates)
+charles_percentage = round(((charles_votes)/ total_votes) * 100, 3)
+diana_percentage = round(((diana_votes)/ total_votes) * 100,3)
+raymon_percentage = round(((raymon_votes)/ total_votes) * 100,3)
+
+# determine winner with conditional logic
+if charles_percentage > diana_percentage and raymon_percentage:
     winner = "Charles Casper Stockham"
-if diane_percentage > raymon_percentage and charles_percentage:
+elif diana_percentage > raymon_percentage and charles_percentage:
     winner  = "Diana DeGette"
-if raymon_percentage > diane_percentage and charles_percentage:
+else: 
     winner = "Raymon Anthony Doane"
 
-print(charles_percentage)
-print(diane_percentage)
-print(raymon_percentage)
+#  print to terminal 
+print("Election Results \n-------------------")
+print(f"Total Votes : {total_votes} \n-------------------"),
+print(f"Charles Casper Stockham: {(charles_percentage)}% ({charles_votes})"),
+print(f"Diana DeGette: {(diana_percentage)}% ({diana_votes})"),
+print(f"Raymon Anthony Doane: {(raymon_percentage)}% ({raymon_votes})"),
+print("-------------------"),
+print(f"Winner : {winner}")
 
+
+# create output file and write the results to it
+output_file = os.path.join ("PyPoll/analysis/Election_results.csv")
+
+with open (output_file, "w", newline = "") as file:
+    writer = csv.writer(file)
+    
+    writer.writerow(["Election Results"])
+    writer.writerow(["---------------------"])
+    writer.writerow([f"Total Votes : {total_votes}"])
+    writer.writerow(["---------------------"])
+    writer.writerow([f"Charles Casper Stockham: {round(charles_percentage,3)}% ({charles_votes})"])
+    writer.writerow([f"Diana DeGette: {(diana_percentage)}% ({diana_votes})"])
+    writer.writerow([f"Raymon Anthony Doane: {(raymon_percentage)}% ({raymon_votes})"])
+    writer.writerow(["---------------------"])
+    writer.writerow([f"Winner : {winner}"])
 
 
 
